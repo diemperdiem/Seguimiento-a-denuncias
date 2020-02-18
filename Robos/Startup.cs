f44a6b1cd4aa82266c8b6e7953e90ee07db49b1f
diemperdiem <38697401+diemperdiem@users.noreply.github.com>
2020-02-18 10:32:37 -0800

    Add project files.

---
+++ Robos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Robos.Startup))]
+namespace Robos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
